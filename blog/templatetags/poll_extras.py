f17a58b760635411b6021cedd2dac325b80d4e7d
Jailyss Neust <kabakovmaksim76@gmail.com>
2022-06-20 02:08:57 +0300

    First Commit

---
+++ blog/templatetags/poll_extras.py
@@ -0,0 +1,25 @@
+
+from django.template.base import TemplateSyntaxError
+# -*- coding: utf-8 -*-
+
+# Стандартный импорт библиотеки шаблонов
+from django import template
+
+# Это чтобы register.filter работал
+register = template.Library()
+
+# Расскажем django о нашем крутом фильтре
+@register.filter
+def rupluralize(value, endings='блог, блога, блогов'):
+    try:
+        endings = endings.split(',')
+        if value % 100 in (11, 12, 13, 14):
+            return endings[2]
+        if value % 10 == 1:
+            return endings[0]
+        if value % 10 in (2, 3, 4):
+            return endings[1]
+        else:
+            return endings[2]
+    except:
+        raise TemplateSyntaxError
